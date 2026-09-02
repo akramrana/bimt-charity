b7419136d809c6d95f05bbf3636c696206160759
akram_rana <akram686202@gmail.com>
2026-09-02 15:05:37 +0600

    changes

--- config/console.php
+++ config/console.php
@@ -11,8 +11,10 @@ $config = [
     'controllerNamespace' => 'app\commands',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
         '@tests' => '@app/tests',
+        '@webroot' => dirname(__DIR__) . '/web',
+        '@web' => '/',
     ],
     'components' => [
         'cache' => [
@@ -29,13 +31,13 @@ $config = [
         'db' => $db,
     ],
     'params' => $params,
-    /*
-    'controllerMap' => [
-        'fixture' => [ // Fixture generation command line.
-            'class' => 'yii\faker\FixtureController',
-        ],
-    ],
-    */
+        /*
+          'controllerMap' => [
+          'fixture' => [ // Fixture generation command line.
+          'class' => 'yii\faker\FixtureController',
+          ],
+          ],
+         */
 ];
 
 if (YII_ENV_DEV) {
